0ec622b9d701608e041b26419cbaa53c2e1a9c6d
george francis <georgefrancis105@gmail.com>
2017-04-22 19:00:35 +1200

    Setup ngMaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
