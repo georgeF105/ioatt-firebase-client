fe42e20e3fcaf69d0e3af6590830ab7d7f92c067
george francis <georgefrancis105@gmail.com>
2017-04-22 22:13:02 +1200

    Make basic device card

--- src/app/devices/devices.module.ts
+++ src/app/devices/devices.module.ts
@@ -5,11 +5,13 @@ import { DevicesRoutingModule } from './devices-routing.module';
 import { AllDevicesComponent } from './all-devices/all-devices.component';
 import { DevicesComponent } from './devices.component';
 import { DeviceCardComponent } from './device-card/device-card.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    DevicesRoutingModule
+    DevicesRoutingModule,
+    MaterialModule
   ],
   declarations: [AllDevicesComponent, DevicesComponent, DeviceCardComponent]
 })
