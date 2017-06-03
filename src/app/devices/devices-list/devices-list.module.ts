d3517d2cf9dcd2f41985445ef5a3a0c69c0c9aca
george francis <georgefrancis105@gmail.com>
2017-06-03 14:54:58 +1200

    Make device-card module

--- src/app/devices/devices-list/devices-list.module.ts
+++ src/app/devices/devices-list/devices-list.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DevicesListComponent } from './devices-list.component';
-import { DeviceCardComponent } from 'app/devices/device-card/device-card.component';
 import { MaterialModule } from '@angular/material';
+
+import { DevicesListComponent } from './devices-list.component';
+import { DeviceCardModule } from '../device-card/device-card.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    DeviceCardModule
   ],
   declarations: [
-    DevicesListComponent,
-    DeviceCardComponent
+    DevicesListComponent
   ],
   exports: [DevicesListComponent]
 })
