cfb198fe2017fe1c305f6293814c05afa41bda9a
george francis <georgefrancis105@gmail.com>
2017-06-11 14:38:14 +1200

    Show conditions

--- src/app/rules/rule-card/rule-card.module.ts
+++ src/app/rules/rule-card/rule-card.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RuleCardComponent } from './rule-card.component';
 import { MdCardModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MdCardModule
+    MdCardModule,
+    MdIconModule
   ],
   declarations: [RuleCardComponent],
   exports: [RuleCardComponent]
