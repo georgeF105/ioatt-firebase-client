552eec5eabbf355434a490114d9552686ef86ed8
george francis <georgefrancis105@gmail.com>
2017-09-02 16:59:06 +1200

    Start making build function. NOT WORKING

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,49 @@
 var functions = require('firebase-functions');
+var express = require('express');
+var path = require('path');
+const gcs = require('@google-cloud/storage')();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+const fs = require('fs');
+const os = require('os');
+
+const app = express();
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
+bucket = functions.config().firebase.storageBucket;
+const tempFilePath = path.join(os.tmpdir(), 'blink.bin');
+
+app.get('/build', (request, response) => {
+  // response.sendFile(path.join(__dirname,'builds/blink.bin'));
+  console.log('starting response');
+  gcs.bucket(bucket).file('blink.bin').download({
+    destination: tempFilePath
+  })
+  .then(() => {
+    console.log('sending response');
+    console.log(tempFilePath);
+    response.sendFile(tempFilePath);
+  })
+  .catch(err => {
+    console.error(err);
+    response.status(500).send(err);
+  });
+});
+
+app.get('/build/debug', (request, response) => {
+  response.json({
+    message: 'debug',
+    body: request.body
+  });
+});
+
+app.get('/build/bucket', (request, response) => {
+  response.send(bucket);
+});
+
+exports.app = functions.https.onRequest(app);
+
 
 // exports.fetchSensorData = functions.database.ref('/sensors/{pushId}/update')
 //     .onWrite(event => {
